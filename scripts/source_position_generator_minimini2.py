2a275bf1626c7e0dc7413fe8557547c26b841746
Kazuki Urabe <urabe0225@gmail.com>
2020-11-19 20:46:04 +0900

    added transmatrix for minimini2

--- scripts/source_position_generator_minimini2.py
+++ scripts/source_position_generator_minimini2.py
@@ -8,6 +8,7 @@ import numpy as np
 from geometry_msgs.msg import PoseStamped
 from rpl.msg import State
 from actionlib_msgs.msg import GoalStatusArray
+from std_msgs.msg import String
 import os
 import datetime
 
@@ -22,35 +23,22 @@ def PoseStampedToText(count, msg):
     buf = buf + str(msg.pose.orientation.w) + "\n"
     return buf
 
-def report():
-    global count
-    #msg1 = rospy.wait_for_message("/amcl_pose", PoseWithCovarianceStamped)
-    msg1 = rospy.wait_for_message("/mavros/local_position/pose", PoseStamped)
-    msg2 = rospy.wait_for_message("/image_raw", Image)
-    msg3 = rospy.wait_for_message("/AR/camera_pose", PoseStamped)
-    buf = PoseStampedToText(count, msg1)
-    with open(pose_file, mode='a') as f:
-        f.write(buf)
-    cv_image = bridge.imgmsg_to_cv2(msg2, desired_encoding='bgr8')
-    cv2.imwrite(path + "/%03.f"%(count)+".png", cv_image)
-    buf = PoseStampedToText(count, msg3)
-    with open(ar_file, mode='a') as f:
-        f.write(buf)
-    count+=1
-    print("RECORDED Source position")
-
 def callback(msg):
-    global prev_status
-    #if not msg.status: return
-    if(msg.status == 2 and prev_status == 1):
-        report()
-    prev_status = msg.status
+    global count
+    if(msg.data == "record"):
+        msg1 = rospy.wait_for_message("/cartographer/pose", PoseStamped)
+        msg2 = rospy.wait_for_message("/AR/camera_pose", PoseStamped)
+        buf = PoseStampedToText(count, msg1)
+        with open(pose_file, mode='a') as f:
+            f.write(buf)
+        buf = PoseStampedToText(count, msg2)
+        count+=1
+        print("RECORDED Source position")
 
 def main():
     try:
         rospy.init_node(NODE_NAME)
-        #rospy.Subscriber("/move_base/status", GoalStatusArray, callback, queue_size=10)
-        rospy.Subscriber("/mission/state", State, callback, queue_size=10)
+        rospy.Subscriber("/AR/create", String, callback, queue_size=10)
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
