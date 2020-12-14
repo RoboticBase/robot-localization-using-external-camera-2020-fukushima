ccf0325c9d1847d3dd98950d721acbab69b0e489
Kazuki Urabe <urabe0225@gmail.com>
2020-12-14 15:59:22 +0900

    modified demo.py to publish /RB/demo_pose

--- scripts/demo.py
+++ scripts/demo.py
@@ -3,7 +3,7 @@
 import rospy
 import numpy as np
 from geometry_msgs.msg import PoseStamped
-from ar_func import read_csv
+from ar_func import read_csv2
 from ar_func import quaternion_to_vector
 from ar_func import PoseStamped_to_Numpyarray
 from ar_func import estimate
@@ -24,10 +24,27 @@ def callback(msg, args):
     p.pose.orientation.w = Est_Quat[3]
     pub.publish(p)
 
+def callback2(msg, args):
+    inputTvec, inputQuat, inputTQ = PoseStamped_to_Numpyarray(msg)
+    translate_matrix = args
+    Est_Quat, Est_pos = estimate(translate_matrix, inputTQ)
+    p = PoseStamped()
+    p.header.frame_id = "map"
+    p.header.stamp = rospy.Time.now()
+    p.pose.position.x = Est_pos[0]
+    p.pose.position.y = Est_pos[1]
+    p.pose.position.z = Est_pos[2]
+    p.pose.orientation.x = Est_Quat[0]
+    p.pose.orientation.y = Est_Quat[1]
+    p.pose.orientation.z = Est_Quat[2]
+    p.pose.orientation.w = Est_Quat[3]
+    pub2.publish(p)
+
 def main():
     try:
         rospy.init_node(NODE_NAME)
         rospy.Subscriber("/AR/estimated_pose", PoseStamped, callback, reverse_matrix, queue_size=10)
+        rospy.Subscriber("/RB/confution_pose", PoseStamped, callback2, reverse_matrix, queue_size=10)
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
@@ -43,6 +60,7 @@ if __name__ == '__main__':
         print(reverse_matrix)
         print(np.dot(translate_matrix, reverse_matrix))
         pub = rospy.Publisher("/AR/demo_pose", PoseStamped, queue_size=10)
+        pub2 = rospy.Publisher("/RB/demo_pose", PoseStamped, queue_size=10)
         main()
 
     except KeyboardInterrupt:
